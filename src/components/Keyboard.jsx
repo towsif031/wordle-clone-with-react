1eae49abae97647deba2933aed361cb5a703a8f9
Towsif Ahmed Omi <towsif031@gmail.com>
2022-05-25 23:43:37 +0600

    Get input from physical keyboard.

--- src/components/Keyboard.jsx
+++ src/components/Keyboard.jsx
@@ -1,3 +1,5 @@
+import { useEffect, useContext, useCallback } from 'react';
+import { AppContext } from '../App';
 import Key from './Key';
 
 const Keyboard = () => {
@@ -5,8 +7,42 @@ const Keyboard = () => {
 	const keys2 = ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'];
 	const keys3 = ['Z', 'X', 'C', 'V', 'B', 'N', 'M'];
 
+	const { onSelectLetter, onEnter, onDelete } = useContext(AppContext);
+
+	const handleKeyboard = useCallback((event) => {
+		if (event.key === 'Enter') {
+			onEnter();
+		} else if (event.key === 'Backspace') {
+			onDelete();
+		} else {
+			keys1.forEach((key) => {
+				if (event.key.toLowerCase() === key.toLowerCase()) {
+					onSelectLetter(key);
+				}
+			});
+			keys2.forEach((key) => {
+				if (event.key.toLowerCase() === key.toLowerCase()) {
+					onSelectLetter(key);
+				}
+			});
+			keys3.forEach((key) => {
+				if (event.key.toLowerCase() === key.toLowerCase()) {
+					onSelectLetter(key);
+				}
+			});
+		}
+	});
+
+	useEffect(() => {
+		document.addEventListener('keydown', handleKeyboard);
+
+		return () => {
+			document.removeEventListener('keydown', handleKeyboard);
+		};
+	}, [handleKeyboard]);
+
 	return (
-		<div className="keyboard">
+		<div className="keyboard" onKeyDown={handleKeyboard}>
 			<div className="line1">
 				{keys1.map((key, i) => (
 					<Key key={i} keyVal={key} />
